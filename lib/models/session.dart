4cae2dfa750aa2f501bacb2c64b6acddbf8d7bb5
Robin Tveråen <robintve@stud.ntnu.no>
2023-02-22 11:02:47 +0100

    add log support for session

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -4,25 +4,31 @@ import 'excercise.dart';
 class Session {
 
   final String name;
-  final List<Excercise> excercises;
+  final List<Excercise>? excercises;
+  final List<Log>? logs;
 
 
 
   Session({
     required this.name,
     required this.excercises,
+    required this.logs,
   });
 
   Session.fromJson(Map<String, Object?> json)
       : name = json['name'] as String,
         excercises = (json['excercises'] as List)
             .map((e) => Excercise.fromJson(e as Map<String, Object?>))
+            .toList(),
+        logs = (json['logs'] as List)
+            .map((e) => Log.fromJson(e as Map<String, Object?>))
             .toList();
 
   Map<String, Object?> toJson() {
     return {
       'name': name,
-      'excercises': excercises.map((e) => e.toJson()).toList(),
+      'excercises': excercises?.map((e) => e.toJson()).toList(),
+      'logs': logs?.map((e) => e.toJson()).toList(),
     };
   }
   }
