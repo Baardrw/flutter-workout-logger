51dbfa2297aa1fe331db86adea6f47801a80632b
Sondre Haneberg <sondrehaneberg@Sondres-MacBook-Pro-2.local>
2023-03-13 20:20:29 +0100

    Connect the search bar with database

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,36 +1,79 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pu_frontend/models/excercise.dart';
 
 import '../common/bottom_bar.dart';
+import '../models/user.dart';
+import '../services/db_service.dart';
 import '../widgets/search_widgets/search_bar.dart';
 
+//Search page where users can search for other users and groups. Contains a TabBarView with
+//options for user search and group search. Each view contains a SearchBar widget, which
+//takes care of the search
 class SearchPage extends StatefulWidget {
-  const SearchPage({super.key});
+  const SearchPage({
+    super.key,
+    this.tile,
+    this.gestureDetectorOnTap,
+  });
+  final String? tile;
+  final Function(Excercise)? gestureDetectorOnTap;
+
 
   @override
   State<SearchPage> createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
+  
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _searchController = TextEditingController();
+    final DatabaseService dbservice = Provider.of<DatabaseService>(context);
+    final TextEditingController _userController = TextEditingController();
+    final TextEditingController _groupController = TextEditingController();
     BottomBar bottomBar = BottomBar(3);
-
+    
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Søk'),
-        backgroundColor: const Color.fromARGB(255, 51, 100, 140),
-      ),
-      body: Column(
-        children: [
-          SearchBar(
-          hintText: "Søk i Logify",
-          controller: _searchController,
+      body: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [Tab(icon: Icon(Icons.person)), Tab(icon: Icon(IconData(0xe9e9, fontFamily: 'MaterialIcons')))]
+            ),
+            title: const Text("Søk"),
+            backgroundColor: const Color.fromARGB(255, 51, 100, 140),
           ),
-        ],
+          body: TabBarView(
+            children: [
+              Column(
+                children: [
+                  SearchBar(
+                    hintText: "Søk blant brukere",
+                    controller: _userController,
+                    dbservice: dbservice,
+                    icon: const Icon(Icons.person, color: Colors.white),
+                    type: "user"
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  SearchBar(
+                    hintText: "Søk blant grupper",
+                    controller: _groupController,
+                    dbservice: dbservice,
+                    icon: const Icon(IconData(0xe9e9, fontFamily: 'MaterialIcons'), color: Colors.white,),
+                    type: "group"
+                  ),
+                ],
+              )
+            ]
+          ),
+        )
       ),
-      bottomNavigationBar: bottomBar.getBar(context),
+      bottomNavigationBar: bottomBar.getBar(context)
     );
   }
 }
--- lib/widgets/search_widgets/search_bar.dart
+++ lib/widgets/search_widgets/search_bar.dart
@@ -1,62 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:pu_frontend/widgets/search_widgets/search_scroller.dart';
+import '../../services/db_service.dart';
 
-class SearchBar extends StatelessWidget {
+//Search widget which includes a search field, a search button and a SearchScroller-widget
+class SearchBar extends StatefulWidget {
+  String type;
   final String hintText;
   final TextEditingController controller;
+  List<Widget> widgets = [];
+  List<Column> searchView = [];
+  Icon icon;
+  DatabaseService dbservice;
   
-  const SearchBar({
+  SearchBar({
     super.key,
     required this.hintText,
-    required this.controller
+    required this.controller,
+    required this.type,
+    required this.icon,
+    required this.dbservice
   });
-
-@override
-Widget build(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.only(
-      top: 15,
-      left: 10.0,
-      right: 10.0,
-    ),
-    child: Row(
-      children: [
-        Expanded(
-          flex: 3,
-          child: TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              suffixIcon: IconButton(
-                onPressed: controller.clear,
-                icon: Icon(Icons.clear),
-              ),
-              prefixIcon: const Icon(Icons.search),
-              enabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(),
-              ),
-              focusedBorder: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey)),
-              hintText: hintText,
-            ),
-          ),
-        ),
-        const SizedBox(width: 5),
-        Expanded(
-          flex: 1,
-          child: ElevatedButton.icon(
-            onPressed: () => print("object"),
-            style: ElevatedButton.styleFrom(
-                maximumSize: const Size(64, 60),
-                backgroundColor: const Color.fromARGB(255, 51, 100, 140),
-                padding: const EdgeInsets.all(20.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0)
-                )),
-            icon: const Icon(IconData(0xf0072, fontFamily: 'MaterialIcons')),
-            label: const Text("Søk"),
-            ),
-        ),
-      ],
-    ),
-  );
+  
+  @override
+  State<SearchBar> createState() => _SearchBarState();
 }
+
+//Updates the SearchScroller whenever the search button is pressed
+void updateSearchScroller(BuildContext context, String controllerText, Future<List<dynamic>> query, SearchBar widget) {
+  widget.searchView = [];
+  widget.searchView.add (
+  Column(
+    children: [
+      FutureBuilder(
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            List<dynamic> searchObjects = snapshot.data as List<dynamic>;
+            if (searchObjects.isEmpty) {
+              return const Text("Ingen resultat");
+            } else {
+              return SearchScroller(searchObjects: searchObjects, icon: widget.icon);
+            }
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        }),
+        future: query,
+      ),
+    ],
+  ));
+}
+
+
+class _SearchBarState extends State<SearchBar> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 15,
+        left: 10.0,
+        right: 10.0,
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: TextField(
+                    controller: widget.controller,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: widget.controller.clear,
+                        icon: const Icon(Icons.clear),
+                      ),
+                      prefixIcon: const Icon(Icons.search),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey)),
+                      hintText: widget.hintText,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        Future<List<dynamic>> query = widget.dbservice.getUsersByUsername(widget.controller.text);
+                        if (widget.type == "user") {
+                          query = widget.dbservice.getUsersByUsername(widget.controller.text);
+                        } else if (widget.type == "group") {
+                          // TODO: declare group query in dbservice
+                        }
+                        updateSearchScroller(context, widget.controller.text, query, widget);
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                        maximumSize: const Size(64, 60),
+                        backgroundColor: const Color.fromARGB(255, 51, 100, 140),
+                        padding: const EdgeInsets.all(20.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0)
+                        )),
+                    icon: const Icon(IconData(0xf0072, fontFamily: 'MaterialIcons')),
+                    label: const Text("Søk"),
+                    ),
+                ),
+              ],
+            ),
+            //ListView dynamically updates whenever the SearchScroller-widget is updated
+            Center(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.searchView.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return widget.searchView[index];
+                }
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
